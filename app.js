b062f2587490dce51394b33bdc0a4aa6fd21e69c
Efraim Munthe <efrayanglain@gmail.com>
2026-05-01 18:12:42 +0700

    finish api

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/calculate", (req, res) => {
-  const { "vehicle-type": vehicleType, ac, laptop } = req.body;
+  const { distance, "vehicle-type": vehicleType, ac, laptop } = req.body;
+
+  // what do we do when < 0? idk
+  if (!isNumber(distance) || !isNumber(ac) || !isNumber(laptop)) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "invalid vehicle distance, ac, or laptop energy usage. Make sure vehicle ac and laptop usage is a valid positive number",
+    });
+  }
 
   if (
     typeof vehicleType !== "string" ||
@@ -22,6 +31,7 @@ app.post("/calculate", (req, res) => {
   }
 
   const { totalEmission, breakdown } = calculateEmission(
+    distance,
     vehicleType,
     ac,
     laptop,
@@ -36,19 +46,44 @@ app.post("/calculate", (req, res) => {
 
 app.listen(3000, () => console.log("listening on http://localhost:3000"));
 
+function isNumber(n) {
+  if (Number.isNaN(n)) return false;
+
+  const parsed = Number(n);
+  return typeof parsed === "number";
+}
+
 /**
  * calculates emission
- * @param vehicleType 'bike' | 'car'
+ * @param distance int
+ * @param vehicleType "motorcycle" | "car"
  * @param acHours int
  * @param laptopHours int
  * */
-function calculateEmission(vehicleType, ac, laptop) {
+function calculateEmission(distance, vehicleType, acHours, laptopHours) {
+  // per km
+  const VEHICLE_COEFFICIENTS = {
+    motorcycle: 0.1,
+    car: 0.2,
+  };
+
+  // per hour usage
+  const DEVICE_COEFFICIENTS = {
+    ac: 0.5,
+    laptop: 0.05,
+  };
+
+  const vehicleEmission = VEHICLE_COEFFICIENTS[vehicleType] * distance;
+  const acEmission = DEVICE_COEFFICIENTS["ac"] * acHours;
+  const laptopEmission = DEVICE_COEFFICIENTS["laptop"] * laptopHours;
+  const totalEmission = vehicleEmission + acEmission + laptopEmission;
+
   return {
-    totalEmission: 10,
+    totalEmission,
     breakdown: {
-      kendaraan: 10,
-      ac: 10,
-      laptop: 10,
+      kendaraan: vehicleEmission,
+      ac: acEmission,
+      laptop: laptopEmission,
     },
   };
 }
