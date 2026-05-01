09ee57869a49d23d8d085c4b4915acf98679955f
Efraim Munthe <efrayanglain@gmail.com>
2026-05-01 18:29:37 +0700

    animate emission progress bar

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 const form = document.getElementById("calculator-form");
+const outputMessage = document.getElementById("output-field");
+const outputTotal = document.getElementById("output-total");
+const barInner = document.getElementById("bar-inner");
 
 if (!form) {
   console.warn("warning: no form found");
@@ -15,6 +18,19 @@ form?.addEventListener("submit", async (e) => {
     body: JSON.stringify(Object.fromEntries(data.entries())),
   });
 
+  outputMessage.innerText = "Awaiting data..";
   const result = await response.json();
   console.log(result);
+
+  if (!result.success) {
+    outputMessage.innerText = `gagal: ${result.message}`;
+  }
+
+  const { breakdown, emisi_total } = result;
+  outputMessage.innerText = `Emisi total mu ${emisi_total}, dengan rincian sebagai berikut: kendaraan: ${breakdown.kendaraan}, ac: ${breakdown.ac}, laptop: ${breakdown.laptop}`;
+
+  const barLength = emisi_total / 15; // x
+  barInner.style.scale = `${barLength} 1`;
+
+  outputTotal.innerText = `${emisi_total} KG C02e`;
 });
